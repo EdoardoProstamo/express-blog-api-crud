670ed853e7cc7624beee12ff1b6e0923aa44e36f
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-01 17:57:44 +0200

    feat: error 500

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const postsRouter = require('./routers/script');
+//error 500
+const errorsCatcher = require('./middlewares/errorsCatcher');
 
 // asset statici
 app.use(express.static('public'));
@@ -11,6 +13,9 @@ app.use(express.json());
 
 // rotta router
 app.use("/script", postsRouter);
+
+//error 500
+app.use(errorsCatcher);
 
 // lancio server
 app.listen (port, () => {
---
+++ middlewares/errorsCatcher.js
@@ -0,0 +1,8 @@
+function errorsCatcher (err, req, res, next) {
+    res.status(500);
+    res.json ({
+        error: err.message,
+    });
+};
+
+module.exports = errorsCatcher;
